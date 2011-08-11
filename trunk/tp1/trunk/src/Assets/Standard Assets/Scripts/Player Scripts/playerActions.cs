6e09af0b2929a30fdb8e7c509e51c61acf25bab1
tommyalvarez <tommyalvarez@d2ab8a96-7ced-9a6a-07b0-5aaa6b481248>
2011-08-11 05:06:07 +0000

    Colocados los invaders. La nave ya tiene movimiento smooth. Se pusieron paredes para evitar que el player se vaya de la pantalla. Falta darle el patron de movimiento horizontal-vertical a los invaders y que lanzen misiles que al colisionar con la nave le saque vida. Falta hacer que si chocan la nave y los invaders directamente pierdas. El menu GUI y algo de puntaje.

--- trunk/tp1/trunk/src/Assets/Standard Assets/Scripts/Player Scripts/playerActions.cs
+++ trunk/tp1/trunk/src/Assets/Standard Assets/Scripts/Player Scripts/playerActions.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 using System.Collections;
 using System.Text;
 
+[RequireComponent(typeof(CharacterController))]
 public class playerActions : MonoBehaviour {
 	private float strafeDirection;
+	private CharacterController playerController;
+	private float speed = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
 	
 	// Use this for initialization
 	void Start () {
+		this.playerController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		strafeDirection = Input.GetAxis ("Horizontal");
-		if (strafeDirection < 0) {
-			 transform.Translate(0.1f, 0, 0);
-		} else if  (strafeDirection > 0) {
-			transform.Translate(-0.1f, 0, 0);
-		} else {
-			transform.Translate(0, 0, 0);
-		}
+		moveDirection = new Vector3(-1.0f*Input.GetAxis("Horizontal"), 0, 0);
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		this.playerController.Move(moveDirection * Time.deltaTime);
 	}
 }
