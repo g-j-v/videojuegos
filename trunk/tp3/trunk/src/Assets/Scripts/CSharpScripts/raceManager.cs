dc93c983c27d9b17c8c997018da7ed0ada6250a9
tommyalvarez <tommyalvarez@d2ab8a96-7ced-9a6a-07b0-5aaa6b481248>
2011-11-08 07:11:23 +0000

    GUI! y logica para ganar practicamente lista. Faltaria posicionar bien el auto cuando arranca el juego, el finish line. Agregarle un collider trigger a la finish line para q dps de q se paso por cada waypoint, tenga q pasar por ahi. Falta decoracion (no se si se pondra). Falta cerrado automatico de la pista...

--- trunk/tp3/trunk/src/Assets/Scripts/CSharpScripts/raceManager.cs
+++ trunk/tp3/trunk/src/Assets/Scripts/CSharpScripts/raceManager.cs
@@ -3,23 +3,55 @@ using System.Collections;
 
 public class raceManager : MonoBehaviour {
 	public GameObject roadCreator;
+	public Texture2D redLight, yellowLight, greenLight;
+	public GameObject carGO;
 	private static int checkPointsDone, checkPointsQty;
-	private bool finishOnTime;
+	private bool finishOnTime, start;
+	private float timeLimit = 60.0f;
 	
 	// Use this for initialization
 	void Start () {
 		roadCreator.GetComponent<RoadCreator>().Generate();
 		checkPointsDone = 0;
 		checkPointsQty = roadCreator.transform.childCount;
+		start = false;
 		Debug.Log(checkPointsQty);
 	}
 	
+	void OnGUI() {
+		if (Time.time < 2.0f) {
+        	GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.05f, Screen.width * 0.10f, Screen.height * 0.20f), redLight);
+		} else if (Time.time < 4.0f) {
+			GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.05f, Screen.width * 0.10f, Screen.height * 0.20f), yellowLight);
+			
+		} else if (Time.time < 6.0f) {
+			GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.05f, Screen.width * 0.10f, Screen.height * 0.20f), greenLight);
+			start = true;
+			carGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+		}
+		
+		GUI.Label(new Rect(Screen.width - Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.1f, Screen.height * 0.1f), "Time Left: " + timeLimit);
+    }
+	
 	// Update is called once per frame
 	void Update () {
-		// Si paso por todos los checkpoints termino a tiempo la carrera (falta lo del tiempo)
-		if (checkPointsDone == checkPointsQty) {
-			finishOnTime = true;
-			Debug.Log("You've Won!");
+		if (start) {
+			if (timeLimit <= 0) {
+				Debug.Log("You've Lost!");
+				finishOnTime = false;
+			}
+			
+			// Si paso por todos los checkpoints termino a tiempo la carrera (falta lo del tiempo)
+			if (checkPointsDone == checkPointsQty) {
+				finishOnTime = true;
+				Debug.Log("You've Won!");
+			} else {
+				timeLimit -= 1.0f * Time.deltaTime;
+			}
+		} else {
+			if (Time.time > 0.7f) {
+				carGO.GetComponent<Rigidbody>().constraints = carGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			}
 		}
 	}
 	
@@ -27,6 +59,8 @@ public class raceManager : MonoBehaviour {
 		if (chkidx - checkPointsDone == 1) {
 			checkPointsDone++;
 			Debug.Log("checkpoint! " + checkPointsDone);
+		} else {
+			Debug.Log("la pifie");
 		}
 	}
 }
